037d0145797a2735649f48f806e239a4174f1de3
lotco <kgsin7@gmail.com>
2019-09-30 18:33:42 +0900

    init

---
+++ exit-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exit-ios
+//
+//  Created by lotco on 2019/09/30.
+//  Copyright © 2019 lotco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
