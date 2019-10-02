a9c1263df11292ce3261cc99c2325b47740dfcd2
lotco <kgsin7@gmail.com>
2019-10-02 16:53:47 +0900

    캘린더 메뉴 추가중

--- exit-ios/Controller/SEMeetingViewController.swift
+++ exit-ios/Controller/SEMeetingViewController.swift
@@ -12,15 +12,10 @@ class SEMeetingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("yaho");
+        
     }
-    
-
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
---
+++ exit-ios/View/SEDateCell.swift
@@ -0,0 +1,13 @@
+//
+//  SEDateCell.swift
+//  exit-ios
+//
+//  Created by lotco on 02/10/2019.
+//  Copyright © 2019 lotco. All rights reserved.
+//
+
+import UIKit
+
+//class SEDateCell: JTAppleCell {
+
+//}
