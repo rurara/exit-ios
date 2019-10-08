a4b7cbc971d89b5c7ff4206c805a931e9826ae7b
lotco <kgsin7@gmail.com>
2019-10-08 12:21:51 +0900

    달력 적용 다시 시도중

--- Podfile
+++ Podfile
@@ -1,12 +1,12 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '13.0'
 
 target 'exit-ios' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for exit-ios
-#  pod 'JTAppleCalendar'
+  pod 'JTAppleCalendar'
   pod 'Alamofire', '~> 5.0.0-rc.2'
 
   target 'exit-iosTests' do
--- exit-ios/Controller/SEMeetingViewController.swift
+++ exit-ios/Controller/SEMeetingViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 import Alamofire
+import JTAppleCalendar
 
 #if di
 #else
 #endif
 
 class SEMeetingViewController: UIViewController {
-
+    @IBOutlet weak var calendarView: JTACMonthView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("yaho");
         
         AF.request("http://localhost:3000/api/v1/test", method:.post, parameters: nil).responseJSON{response in print(response)}
     }
-    /*
+}
+
+//JTACMonthViewDelegate,
+extension SEMeetingViewController:  JTACMonthViewDataSource {
+//    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+//        <#code#>
+//    }
+//
+//    func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
+//        let myCustomCell = calendar.dequeueReusableCell(withReuseIdentifier: "dateCell", for: indexPath) as! CellView
+//        configureVisibleCell(myCustomCell: myCustomCell, cellState: cellState, date: date, indexPath: indexPath)
+//        return myCustomCell
+//    }
+    
+    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        let startDate = formatter.date(from: "2018 01 01")!
+        let endDate = Date()
+        return ConfigurationParameters(startDate: startDate, endDate: endDate)
+    }
 }
