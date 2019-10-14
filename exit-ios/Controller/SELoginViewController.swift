31546c35f9e2475667c3230f93915a4f2ada5e3d
lotco <kgsin7@gmail.com>
2019-10-14 17:24:38 +0900

    로그인 컨트롤러 연결

--- exit-ios/Controller/SELoginViewController.swift
+++ exit-ios/Controller/SELoginViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class SELoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBAction func userRegisterButtonAction(_ sender: Any) {
+        
+        print("register", self.emailTextField.text! , self.passwordTextField.text!, self.userNameTextField.text!);
+    }
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+        print("login", self.emailTextField.text! , self.passwordTextField.text!, self.userNameTextField.text!);
+    }
+    
+    
     @IBAction func closeButtonAction(_ sender: Any) {
         self.dismiss(animated: true) {
-            print("login view close");
+            print("login view close", self.emailTextField.text! , self.passwordTextField.text!, self.userNameTextField.text!);
         }
     }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("login view")
