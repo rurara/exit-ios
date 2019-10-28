51f483aefe8e727c3d47dcde0e6099f80064a75a
lotco <kgsin7@gmail.com>
2019-10-28 18:32:39 +0900

    유저 모델 추가

---
+++ exit-ios/Model/SEUser.swift
@@ -0,0 +1,19 @@
+//
+//  SEUser.swift
+//  exit-ios
+//
+//  Created by lotco on 28/10/2019.
+//  Copyright © 2019 lotco. All rights reserved.
+//
+
+import UIKit
+
+class SEUser: NSObject {
+    var userToken:String!;
+    var userName:String!;
+    var userPoint:NSNumber!;
+    
+    override init() {
+        print("user init!");
+    }
+}
